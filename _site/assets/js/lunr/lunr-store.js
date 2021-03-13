d6f8e3b6d0bc3f6ff5474042f6301234d749536b
JohnHuong <lvh8998@gmail.com>
2021-03-14 05:52:00 +0700

    hlv

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/14/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Python is cool from Huyen Chip",
+        "excerpt":"A gentle guide to the Python features that I didn’t know existed or was too afraid to use. This will be updated as I learn more and become less lazy. This uses python &gt;= 3.6. GitHub has problem rendering Jupyter notebook so I copied the content here. I still keep...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/14/hello-world.html",
+        "teaser": null
+      }]
