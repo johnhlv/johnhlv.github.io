7141559aa13d6e5347a5809ce4d6dbbcb04c9d8f
JohnHuong <lvh8998@gmail.com>
2021-03-17 23:44:01 +0700

    test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "Chapter 01: Personal Pronouns, Possessive Pronouns, and Reflexive Pronouns",
+        "excerpt":"Đại từ nhân xưng Person   Singular     Plural     Chủ ngữ Tân ngữ Nghĩa Chủ ngữ Tân ngữ Nghĩa 1st I me tôi We us chúng tôi 2nd You you bạn You you các bạn 3rd He She It him her it anh ấy chị ấy nó...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter01.html",
+        "teaser": null
+      },{
+        "title": "Chapter 02: Countable and Uncountable Nouns, Quantifiers",
+        "excerpt":"Danh từ đếm được và danh từ không đếm được     Cách thành lập danh từ số nhiều         Số ít Số nhiều Cách thêm             Normal         book pencil books pencils Thêm s vào      ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter02.html",
+        "teaser": null
+      },{
+        "title": "Chapter 03: Compound Nouns and Genitive and Possessive Case",
+        "excerpt":"Danh từ ghép Cách thành lập danh từ ghép             Form Example           Danh từ + Danh từ armchair, bath-tub, newspaper           Tính từ + Danh từ mineral water, greenhouse           Danh động...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter03.html",
+        "teaser": null
+      },{
+        "title": "Chapter 04: Articles",
+        "excerpt":"Mạo từ không xác định: A/An Dùng trước danh từ đếm được số ít Vị trí A đứng trước danh từ bắt đầu bằng 1 phụ âm hoặc 1 nguyên âm nhưng được phát âm như phụ âm Ex: a month, a uniform, a horse,… An đứng trước danh từ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter04.html",
+        "teaser": null
+      },{
+        "title": "Chapter 05: Prepositions",
+        "excerpt":"Giới từ chỉ thời gian   Nghĩa Cách dùng other       In trong, vào chỉ các buổi trong ngày trừ night (at night) chỉ tháng, năm, mùa, thập niên, thế kỷ hoặc khoảng thời gian ở tương lai In time - đúng lúc, kịp lúc      ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter05.html",
+        "teaser": null
+      },{
+        "title": "Chapter 06: Phrasal Verbs ",
+        "excerpt":"Cụm động từ Là một động từ kết hợp với giới từ, trạng từ hoặc đôi khi cả hai để tạo ra một động từ mới, thường có nghĩa khác với động từ chính. Ex: Run into - gặp, va phải; Take of - cởi ra; Look foward to -...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter06.html",
+        "teaser": null
+      },{
+        "title": "Chapter 07: Conjunctions",
+        "excerpt":"Liên từ kết hợp Chức năng Nối từ, cụm từ hoặc mệnh đề độc lập có cùng chức năng ngữ pháp               And But Or Nor       So Yet For …       Liên từ tương quan Cặp liên từ tương...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter07.html",
+        "teaser": null
+      },{
+        "title": "Chapter 08: Subject - Verb Agreement",
+        "excerpt":"CÔNG THỨC Note Updating!           Singular subject + Singular verb Plural subject + Plural verb             Noun + and + Noun + Plural verb Noun + and + Noun + Singular verb Khi các danh từ đề cập tới người/vật khác...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter08.html",
+        "teaser": null
+      },{
+        "title": "Chapter 09: Tenses",
+        "excerpt":"Hiện Tại Cách sử dụng Dấu hiệu Chú ý tẹo       Đơn Chân lí, sự thật hiển nhiên, thời khóa biểu, lịch trình theo thời gian cố định Thói quen, sự việc lặp đi lặp lại Nhận thức, trạng thái, cảm giác ở thời điểm hiện tại Dùng...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter09.html",
+        "teaser": null
+      },{
+        "title": "Chapter 10: Modal Verbs",
+        "excerpt":"Can - Can’t Nghĩa: có thể,… Khả năng hoặc cơ hội ở hiện tại hoặc tương lai Sự xin phép, cho phép Lời yêu cầu, đề nghị, gợi ý Khả năng có thể xảy ra hoặc dự đoán Could - Couldn’t Nghĩa: có thế,… là quá khứ của Can Khả...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter10.html",
+        "teaser": null
+      }]
