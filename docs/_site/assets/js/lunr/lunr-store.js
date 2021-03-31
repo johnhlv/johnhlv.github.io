c388262241fa3b571ca7d52a410ecbc1c786e823
JohnHuong <lvh8998@gmail.com>
2021-04-01 06:31:53 +0700

    CaThangTu

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "Chapter 01: Personal Pronouns, Possessive Pronouns, and Reflexive Pronouns",
+        "excerpt":"Đại từ nhân xưng Person   Singular     Plural     Chủ ngữ Tân ngữ Nghĩa Chủ ngữ Tân ngữ Nghĩa 1st I me tôi We us chúng tôi 2nd You you bạn You you các bạn 3rd He She It him her it anh ấy chị ấy nó...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter01.html",
+        "teaser": null
+      },{
+        "title": "Chapter 02: Countable and Uncountable Nouns, Quantifiers",
+        "excerpt":"Danh từ đếm được và danh từ không đếm được     Cách thành lập danh từ số nhiều         Số ít Số nhiều Cách thêm             Normal         book pencil books pencils Thêm s vào      ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter02.html",
+        "teaser": null
+      },{
+        "title": "Chapter 03: Compound Nouns and Genitive and Possessive Case",
+        "excerpt":"Danh từ ghép Cách thành lập danh từ ghép             Form Example           Danh từ + Danh từ armchair, bath-tub, newspaper           Tính từ + Danh từ mineral water, greenhouse           Danh động...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter03.html",
+        "teaser": null
+      },{
+        "title": "Chapter 04: Articles",
+        "excerpt":"Mạo từ không xác định: A/An Dùng trước danh từ đếm được số ít Vị trí A đứng trước danh từ bắt đầu bằng 1 phụ âm hoặc 1 nguyên âm nhưng được phát âm như phụ âm Ex: a month, a uniform, a horse,… An đứng trước danh từ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter04.html",
+        "teaser": null
+      },{
+        "title": "Chapter 05: Prepositions",
+        "excerpt":"Giới từ chỉ thời gian   Nghĩa Cách dùng other       In trong, vào chỉ các buổi trong ngày trừ night (at night) chỉ tháng, năm, mùa, thập niên, thế kỷ hoặc khoảng thời gian ở tương lai In time - đúng lúc, kịp lúc      ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter05.html",
+        "teaser": null
+      },{
+        "title": "Chapter 06: Phrasal Verbs ",
+        "excerpt":"Cụm động từ Là một động từ kết hợp với giới từ, trạng từ hoặc đôi khi cả hai để tạo ra một động từ mới, thường có nghĩa khác với động từ chính. Ex: Run into - gặp, va phải; Take of - cởi ra; Look foward to -...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter06.html",
+        "teaser": null
+      },{
+        "title": "Chapter 07: Conjunctions",
+        "excerpt":"Liên từ kết hợp Chức năng Nối từ, cụm từ hoặc mệnh đề độc lập có cùng chức năng ngữ pháp               And But Or Nor       So Yet For …       Liên từ tương quan Cặp liên từ tương...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter07.html",
+        "teaser": null
+      },{
+        "title": "Chapter 08: Subject - Verb Agreement",
+        "excerpt":"CÔNG THỨC         Singular subject + Singular verb Plural subject + Plural verb         Noun + and + Noun + Plural verb Noun + and + Noun + Singular verb         Every/ Each + Singular noun + and + Every/ Each...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter08.html",
+        "teaser": null
+      },{
+        "title": "Chapter 09: Tenses",
+        "excerpt":"Hiện Tại Cách sử dụng Dấu hiệu Chú ý tẹo       Đơn Chân lí, sự thật hiển nhiên, thời khóa biểu, lịch trình theo thời gian cố định Thói quen, sự việc lặp đi lặp lại Nhận thức, trạng thái, cảm giác ở thời điểm hiện tại Dùng...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter09.html",
+        "teaser": null
+      },{
+        "title": "Chapter 10: Modal Verbs",
+        "excerpt":"Can - Can’t Nghĩa: có thể,… Khả năng hoặc cơ hội ở hiện tại hoặc tương lai Sự xin phép, cho phép Lời yêu cầu, đề nghị, gợi ý Khả năng có thể xảy ra hoặc dự đoán Could - Couldn’t Nghĩa: có thể,… là quá khứ của Can Khả...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/16/Chapter10.html",
+        "teaser": null
+      },{
+        "title": "Chapter 11: Conditional Sentences",
+        "excerpt":"Gồm 2 mệnh đề IF CLAUSE: Mệnh đề chỉ điều kiện MAIN CLAUSE: Mệnh đề chính chỉ kết quả Có 3 loại Điều kiện loại 1: Có thật trong hiện tại hoặc tương lai Meaning: Có thể thực hiện trong hiện tại hoặc tương lai IF CLAUSE: Present tense MAIN...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter11.html",
+        "teaser": null
+      },{
+        "title": "Chapter 12: Passive Form",
+        "excerpt":"Câu bị động là câu trong đó chủ ngữ là người hay vật nhận hoặc chịu tác động của hành động Cách đổi sang câu bị động           Present simple am/ is/ are + past-participle       Present continuous am/ is/ are + being +...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter12.html",
+        "teaser": null
+      },{
+        "title": "Chapter 13: Reporter Speech",
+        "excerpt":"Lời nói gián tiếp là lời tường thuật lại ý của người nói, đôi khi không cần phải dùng đúng những từ của người nói Cách đổi từ câu trực tiếp sang câu gián tiếp Câu trần thuật Dùng động từ giới thiệu say hoặc tell: say(that), say to somebody(that),...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter13.html",
+        "teaser": null
+      },{
+        "title": "Chapter 14: Gerunds and Infinitives",
+        "excerpt":"Danh động từ Danh động từ là hình thức động từ thêm -ing Vai trò Chủ ngữ Swimming is good for our health Tân ngữ Danh động từ làm tân ngữ trực tiếp sau các động từ bên dưới           admit - thú nhận appreciate -...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter14.html",
+        "teaser": null
+      },{
+        "title": "Chapter 15: Comparison of Adjectives and Advebs",
+        "excerpt":"So sánh bằng AS + adjective/ adverb + AS Not AS/ SO + adjective/ adverb + AS As much/ many(+noun) As Được dùng để nói về số lượng The same(+noun) As Jenie’s salary is the same as mine Jenie gets the same salary as me So sánh hơn So sánh...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter15.html",
+        "teaser": null
+      },{
+        "title": "Chapter 16: Relative Clauses",
+        "excerpt":"Mệnh đề quan hệ - Relative Clause hay còn gọi là mệnh đề tính ngữ - Adjective Clause, được dùng như một tính từ để bổ nghĩa cho danh từ đứng trước Mệnh đê quan hệ được đặt ngay sau danh từ mà nó bổ nghĩa và được bắt đầu...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter16.html",
+        "teaser": null
+      },{
+        "title": "Chapter 17: Adverb Clausses",
+        "excerpt":"Mệnh đề trạng ngữ chỉ thời gian Mệnh đề trạng ngữ chỉ thời gian bắt đầu bằng       when - khi as - trong khi, khi as long as - tới khi since - từ khi until/ til - cho đến khi while - trong khi whenever -...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter17.html",
+        "teaser": null
+      },{
+        "title": "Chapter 18: Noun Clausses",
+        "excerpt":"Là mệnh đề phụ có chức năng như một danh từ Thường bắt đầu bằng that, whether, if who, whose, what, when, where, why, how Mệnh đề danh từ có thể làm Chủ ngữ Why he left is not our concern Tân ngữ của động từ hoặc giới từ I...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter18.html",
+        "teaser": null
+      },{
+        "title": "Chapter 19: Word Formation and Word Order",
+        "excerpt":"Cách thành lập từ - Word Formations Thành lập danh từ - Noun formations Một số danh từ được thành lập bằng cách thêm các hậu tố - suffixes vào sau động từ - verb, danh từ – noun hoặc tính từ – adj VERB + ment/ al invest -...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter19.html",
+        "teaser": null
+      },{
+        "title": "Chapter 20: Questions",
+        "excerpt":"Câu hỏi Yes-No Yes-No Questions bắt đầu bằng Một trợ động từ: do, be, have, modal verbs Hoặc động từ to be CT Auxiliary/ Be + Subject …? Câu hỏi WH WH-Questions bắt đầu bằng các nghi vấn từ - question words who, whom, whose, where, when, which, what, why,...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter20.html",
+        "teaser": null
+      },{
+        "title": "Chapter 21: Phrases",
+        "excerpt":"Cụm từ là một nhóm từ hay đoạn câu mà những từ trong đó có liên hệ với nhau Cụm động từ nguyên mẫu - Infinitive phrase Bắt đầu bằng một động từ nguyên mẫu Infinitive phrase được dùng như Danh từ - Noun Tính từ - Adjective Trạng từ...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter21.html",
+        "teaser": null
+      },{
+        "title": "Chapter 22: Inversion",
+        "excerpt":"Phép đảo của động từ là sự đảo ngược vị trí giữa động từ/ trợ động từ và chủ ngữ(nếu trong câu không có trợ động từ thì phải mượn trợ động từ to do) Hình thức đảo ngữ được dùng khi Trạng từ, trạng ngữ phủ định hoặc các...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/03/26/Chapter22.html",
+        "teaser": null
+      }]
---
+++ docs/index.markdown
@@ -0,0 +1,13 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+layout: home
+author_profile: true
+sidebar:
+  nav: "docs"
+---
+
+# Big Hero 6
+
+![Bighero6](./aboutme/hero.jpg)
